6c764ae683a858bba83dc575f65026e3f07e042b
Rajamahanthi Sandeep <31082@ITCINFOTECH.COM>
2019-09-27 10:57:24 +0530

    push nca

---
+++ NCA/NCA/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NCA.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
